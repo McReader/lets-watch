28ac024da6d916f4b7c80356b929ec2d33a3af24
a.dzeshko@gmail.com <a.dzeshko@gmail.com@1075ef40-6d70-ef33-689f-32a8824456a6>
2013-03-04 18:04:49 +0000

    1. Added and used revised AsyncTask. 2. Implemented ViewHolder pattern.

--- trunk/ lets-watch/Let'sWatch!/src/com/sickfuture/letswatch/adapter/BoxOfficeCursorAdapter.java
+++ trunk/ lets-watch/Let'sWatch!/src/com/sickfuture/letswatch/adapter/BoxOfficeCursorAdapter.java
@@ -3,6 +3,7 @@ package com.sickfuture.letswatch.adapter;
 import android.content.Context;
 import android.database.Cursor;
 import android.support.v4.widget.CursorAdapter;
+import android.text.TextUtils;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -14,44 +15,55 @@ import com.sickfuture.letswatch.images.ImageLoader;
 
 public class BoxOfficeCursorAdapter extends CursorAdapter {
 
-	private TextView mTitleTextView, mCriticsConsensusTextView;
+/*	private TextView mTitleTextView, mCriticsConsensusTextView;
 
 	private ImageView mPosterImageView;
 
-	public BoxOfficeCursorAdapter(Context context, Cursor c) {
+*/	public BoxOfficeCursorAdapter(Context context, Cursor c) {
 		super(context, c, true);
 	}
 
+	//Basic @getView method where implemented ViewHolder
+	//It uses to avoid calling @findViewById every time
+	@Override
+	public View getView(int position, View convertView, ViewGroup parent) {
+		if (!mDataValid) {
+			throw new IllegalStateException("this should only be called when the cursor is valid");
+		}
+		if (!mCursor.moveToPosition(position)) {
+			throw new IllegalStateException("couldn't move cursor to position " + position);
+		}
+		View view;
+		if (convertView == null) {
+			view = newView(mContext, mCursor, parent);
+			ViewHolder holder = new ViewHolder();
+			holder.mTitleTextView = (TextView) view.findViewById(R.id.box_office_title_text_view);
+			holder.mCriticsConsensusTextView = (TextView) view.findViewById(R.id.box_office_critics_consensus_text_view);
+			holder.mPosterImageView = (ImageView) view.findViewById(R.id.box_office_poster_image_view);
+			view.setTag(R.string.view_holder, holder);
+		} else {
+			view = convertView;
+		}
+		bindView(view, mContext, mCursor);
+		return view;
+	}
+
+
 	@Override
 	public void bindView(View view, Context context, Cursor cursor) {
-		mTitleTextView = (TextView) view
-				.findViewById(R.id.box_office_title_text_view);
-		mCriticsConsensusTextView = (TextView) view
-				.findViewById(R.id.box_office_critics_consensus_text_view);
-		mPosterImageView = (ImageView) view
-				.findViewById(R.id.box_office_poster_image_view);
-		if (!cursor.getString(
-				cursor.getColumnIndex(Contract.BoxOfficeColumns.POSTERS))
-				.equals("")) {
-			ImageLoader
-					.getInstance()
-					.bind(this,
-							mPosterImageView,
-							cursor.getString(cursor
-									.getColumnIndex(Contract.BoxOfficeColumns.POSTERS)));
+		ViewHolder holder = (ViewHolder) view.getTag(R.string.view_holder);
+		if (!TextUtils.isEmpty(cursor.getString(cursor.getColumnIndex(Contract.BoxOfficeColumns.POSTERS)))) {
+			ImageLoader.getInstance().bind(this,
+							holder.mPosterImageView,cursor.getString(cursor.getColumnIndex(Contract.BoxOfficeColumns.POSTERS)));
 		}
-		mTitleTextView.setText(cursor.getString(cursor
-				.getColumnIndex(Contract.BoxOfficeColumns.MOVIE_TITLE)));
-		if (!cursor
-				.getString(
-						cursor.getColumnIndex(Contract.BoxOfficeColumns.CRITICS_CONSENSUS))
-				.equals("")) {
-			mCriticsConsensusTextView.setVisibility(View.VISIBLE);
-			mCriticsConsensusTextView
-					.setText(cursor.getString(cursor
-							.getColumnIndex(Contract.BoxOfficeColumns.CRITICS_CONSENSUS)));
+		holder.mTitleTextView.setText(cursor.getString(cursor.getColumnIndex(Contract.BoxOfficeColumns.MOVIE_TITLE)));
+		if (!TextUtils.isEmpty(cursor.getString(
+						cursor.getColumnIndex(Contract.BoxOfficeColumns.CRITICS_CONSENSUS)))) {
+			holder.mCriticsConsensusTextView.setVisibility(View.VISIBLE);
+			holder.mCriticsConsensusTextView
+					.setText(cursor.getString(cursor.getColumnIndex(Contract.BoxOfficeColumns.CRITICS_CONSENSUS)));
 		} else {
-			mCriticsConsensusTextView.setVisibility(View.GONE);
+			holder.mCriticsConsensusTextView.setVisibility(View.GONE);
 		}
 	}
 
@@ -60,5 +72,13 @@ public class BoxOfficeCursorAdapter extends CursorAdapter {
 		View view = View.inflate(context, R.layout.adapter_box_office, null);
 		return view;
 	}
+	
+	// ViewHolder pattern implementation class
+	static class ViewHolder{
+		TextView mTitleTextView, mCriticsConsensusTextView;
+
+		ImageView mPosterImageView;
+	}
+
 
 }
