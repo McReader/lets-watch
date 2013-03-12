55fbd1db49a3101639f921a4c440a2fdbbe7a184
mcreader215 <mcreader215@gmail.com>
2013-03-12 20:19:02 +0000

    new CommonDataBase

---  lets-watch/Let'sWatch!/src/com/sickfuture/letswatch/database/CommonDataBase.java
+++  lets-watch/Let'sWatch!/src/com/sickfuture/letswatch/database/CommonDataBase.java
@@ -1,16 +1,29 @@
 package com.sickfuture.letswatch.database;
 
-import com.sickfuture.letswatch.ContextHolder;
+import java.lang.reflect.Method;
+import java.security.AccessController;
+import java.security.PrivilegedActionException;
+import java.security.PrivilegedExceptionAction;
 
-import android.content.Context;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
-import android.database.sqlite.SQLiteDatabase.CursorFactory;
 import android.database.sqlite.SQLiteOpenHelper;
 import android.util.Log;
 
+import com.sickfuture.letswatch.ContextHolder;
+import com.sickfuture.letswatch.content.contract.Contract;
+import com.sickfuture.letswatch.content.contract.TableGetters;
+
 public class CommonDataBase extends SQLiteOpenHelper {
+
+	private static final String GET_COLUMNS = "getColumns";
+
+	private static final String GET_TABLE_NAME = "getTableName";
 
 	private static final String SELECT_DISTINCT_TBL = "select DISTINCT tbl_name from sqlite_master where tbl_name = '";
 
@@ -18,57 +31,40 @@ public class CommonDataBase extends SQLiteOpenHelper {
 
 	private static final String CREATE_TABLE = "CREATE TABLE";
 
-	private static final String VARCHAR_PRIMARY_KEY = " VARCHAR PRIMARY KEY";
+	private static final String INTEGER_PRIMARY_KEY = " INTEGER PRIMARY KEY";
 
 	private static final String DATABASE_NAME = "moviesinfo.store.db";
 
-	protected static final String DROP_TABLE = "DROP TABLE IF EXISTS ";
+	private static final String DROP_TABLE = "DROP TABLE IF EXISTS ";
 
 	private final static int DB_VERSION = 3;
 
-	private CreateAndUpgrade createAndUpgrade;
+	private static SQLiteDatabase mDatabase;
 
-	protected CommonDataBase(Context context, CursorFactory factory,
-			final String tableName, final String[] coloumns) {
-		super(context, DATABASE_NAME, factory, DB_VERSION);
-		createAndUpgrade = new CreateAndUpgrade() {
+	private static volatile CommonDataBase instance;
 
-			public void open(SQLiteDatabase db) {
-				if (!isTableExists(db, tableName)) {
-					onCreate(db);
+	private CommonDataBase() {
+		super(ContextHolder.getInstance().getContext(), DATABASE_NAME, null,
+				DB_VERSION);
+	}
+
+	public static CommonDataBase getInstance() {
+		CommonDataBase localInstance = instance;
+		if (instance == null) {
+			synchronized (CommonDataBase.class) {
+				localInstance = instance;
+				if (localInstance == null) {
+					instance = localInstance = new CommonDataBase();
 				}
 			}
-
-			public void upgrade(SQLiteDatabase db, int oldVersion,
-					int newVersion) {
-				try {
-					db.beginTransaction();
-					db.execSQL(DROP_TABLE + tableName);
-					db.setTransactionSuccessful();
-				} finally {
-					db.endTransaction();
-				}
-				onCreate(db);
-			}
-
-			public void create(SQLiteDatabase db) {
-				try {
-					db.beginTransaction();
-					db.execSQL(creationTable(tableName, coloumns));
-					Log.d("DB TABLE CREATION",
-							"create" + creationTable(tableName, coloumns));
-					db.setTransactionSuccessful();
-				} finally {
-					db.endTransaction();
-				}
-			}
-		};
+		}
+		return localInstance;
 	}
 
 	private String creationTable(String tableName, String[] coloumns) {
 		StringBuilder sb = new StringBuilder();
 		sb.append(CREATE_TABLE + " " + tableName + " (");
-		sb.append(coloumns[0] + VARCHAR_PRIMARY_KEY + ", ");
+		sb.append(coloumns[0] + INTEGER_PRIMARY_KEY + ", ");
 		for (int i = 1; i < coloumns.length - 1; i++) {
 			sb.append(coloumns[i] + " " + VARCHAR + ", ");
 		}
@@ -79,28 +75,142 @@ public class CommonDataBase extends SQLiteOpenHelper {
 	@Override
 	public void onOpen(SQLiteDatabase db) {
 		super.onOpen(db);
-		createAndUpgrade.open(db);
+		/*if (!isTableExists(db, mTableName)) {
+			onCreate(db);
+		}*/
 	}
 
+	/** Note to self: Don't forget delete logs */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
 	@Override
 	public void onCreate(SQLiteDatabase db) {
-		createAndUpgrade.create(db);
+		Class<TableGetters>[] subClasses = (Class<TableGetters>[]) Contract.class
+				.getClasses();
+		Log.i("REFLECTION TEST", "Class Array Size:  " + subClasses.length);
+		String tableName=null;
+		String[] columns=null; 
+		for (Class<TableGetters> cls : subClasses) {
+			try {
+				Class cl = Class.forName(cls.getName());
+				final Object obj = cl.newInstance();
+				final Method getTableMethod = cl.getDeclaredMethod(GET_TABLE_NAME);
+				final Method getColumnsMethod = cl.getDeclaredMethod(GET_COLUMNS);
+				tableName = AccessController
+						.doPrivileged(new PrivilegedExceptionAction() {
+							public Object run() throws Exception {
+								if (!getTableMethod.isAccessible()) {
+									getTableMethod.setAccessible(true);
+								}
+								return getTableMethod.invoke(obj);
+							}
+						});
+				columns = AccessController
+						.doPrivileged(new PrivilegedExceptionAction() {
+							public Object run() throws Exception {
+								if (!getColumnsMethod.isAccessible()) {
+									getColumnsMethod.setAccessible(true);
+								}
+								return getColumnsMethod.invoke(obj);
+							}
+						});
+				Log.i("REFLECTION TEST", "class:  " + cls.getName());
+				Log.i("REFLECTION TEST", "result:  " + tableName);
+				Log.i("REFLECTION TEST", "result:  " + columns.length);
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+			} catch (ClassNotFoundException e) {
+				e.printStackTrace();
+			} catch (NoSuchMethodException e) {
+				e.printStackTrace();
+			} catch (IllegalAccessException e) {
+				e.printStackTrace();
+			} catch (InstantiationException e) {
+				e.printStackTrace();
+			} catch (PrivilegedActionException e) {
+				e.printStackTrace();
+			}
+		
+		try {
+			db.beginTransaction();
+			db.execSQL(creationTable(tableName, columns));
+			Log.d("DB TABLE CREATION",
+					"create" + creationTable(tableName, columns));
+			db.setTransactionSuccessful();
+		} finally {
+			db.endTransaction();
+		}
+		}
 	}
 
 	@Override
 	public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-		createAndUpgrade.upgrade(db, oldVersion, newVersion);
+		//TODO Do onUpgrade like onCreate
+		try {
+			db.beginTransaction();
+			//db.execSQL(DROP_TABLE + mTableName);
+			db.setTransactionSuccessful();
+		} finally {
+			db.endTransaction();
+		}
+		onCreate(db);
 	}
 
-	protected void delete(String tableName, String where, String[] whereArgs) {
-		if (isTableExists(getWritableDatabase(), tableName)) {
-			getWritableDatabase().delete(tableName, where, whereArgs);
+	public long addItem(String tableName, String[] columns,
+			JSONObject jsonObject, ContentValues contentValues)
+			throws JSONException {
+		mDatabase = getWritableDatabase();
+		long value;
+		try {
+			mDatabase.beginTransaction();
+			value = mDatabase.insertWithOnConflict(tableName, null, contentValues,
+					SQLiteDatabase.CONFLICT_REPLACE);
+			if (value <= 0) {
+				throw new SQLException("Failed to insert row into " + tableName);
+			}
+			mDatabase.setTransactionSuccessful();
+		} finally {
+			mDatabase.endTransaction();
+		}
+		return value;
+	}
+
+	public Cursor getItems(String tableName, String[] columns, String orderBy,
+			String selection, String[] selectionArgs) {
+		mDatabase = getReadableDatabase();
+		Cursor cursor;
+		try {
+			mDatabase.beginTransaction();
+			cursor = null;
+			cursor = mDatabase.query(tableName, null, selection, selectionArgs,
+					null, null, orderBy);
+			if (cursor == null) {
+				throw new SQLException("Failed to query row from " + tableName);
+			}
+			mDatabase.setTransactionSuccessful();
+		} finally {
+			mDatabase.endTransaction();
+		}
+		return cursor;
+	}
+
+	protected static void delete(String tableName, String where,
+			String[] whereArgs) {
+		if (isTableExists(mDatabase, tableName)) {
+			mDatabase.delete(tableName, where, whereArgs);
 		}
 	}
 
-	private boolean isTableExists(SQLiteDatabase database, String tableName) {
-		Cursor cursor = database.rawQuery(
-				SELECT_DISTINCT_TBL + tableName + "'", null);
+	private static boolean isTableExists(SQLiteDatabase database,
+			String tableName) {
+		database.beginTransaction();
+		Cursor cursor;
+		try {
+			cursor = database.rawQuery(SELECT_DISTINCT_TBL + tableName + "'",
+					null);
+			database.setTransactionSuccessful();
+		} finally {
+			database.endTransaction();
+		}
 		if (cursor.getCount() > 0) {
 			cursor.close();
 			return true;
